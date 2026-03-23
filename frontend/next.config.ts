caa1dd626aa8b37458e85b2bb25188f2ed881fbe
Ripu <ripudaman600@gmail.com>
2026-03-23 16:19:37 +1100

    Initial commit: Digital Twin infrastructure and application

---
+++ frontend/next.config.ts
@@ -0,0 +1,19 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
+};
+
+export default nextConfig;
